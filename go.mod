b3c7b364a1a7323fe615cb2fe2b188b2a98d45be
drazidi <dreidedreide209@gmail.com>
2021-11-03 09:29:58 -0800

    membuat app sapa halo sederhana

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/drazidi/app-sapa-hello
+
+go 1.17
+
+require github.com/drazidi/sapa-halo/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	sapa_halo "github.com/drazidi/sapa-halo/v2"
+)
+
+func main() {
+	fmt.Println(sapa_halo.SapaHalo("Fulan"))
+}
